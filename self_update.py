7faf892e0b1471dfee39e0f8fe1327602e2311bd
hexbat <hexbat@gmail.com>
2016-09-15 13:49:33 +0300

    check hashes

--- self_update.py
+++ self_update.py
@@ -1,4 +1,28 @@
 #python3
 
-import subprocess
+from subprocess import Popen, PIPE, call
 import git
+
+def main():
+  cmd = 'git rev-parse HEAD'
+  p = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
+  out, err = p.communicate()
+  if out:
+    local_hash = out
+  if err:
+    print 'Error while getting local hash'
+  cmd = 'git ls-remote https://github.com/hexbat/test/test_self_update.git HEAD'
+  p = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
+  out, err = p.communicate()
+  if out:
+    remote_hash = out
+  if err:
+    print 'Error while getting remote hash'
+  
+  print local_hash
+  print remote_hash
+  
+
+if __name__ == "__main__":
+    main()
+
