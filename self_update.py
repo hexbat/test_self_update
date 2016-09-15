3d45118e68153e6bcab35cce579a4dea9598c4de
hexbat <hexbat@gmail.com>
2016-09-15 14:44:39 +0300

    compare hashes

--- self_update.py
+++ self_update.py
@@ -5,27 +5,33 @@ from subprocess import Popen, PIPE, call
 import git
 
 def main():
-  cmd = 'git rev-parse HEAD'
-  p = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
-  out, err = p.communicate()
-  if out:
-    local_hash = out.split()[0]
-  if err:
-    print('Error while getting local hash')
-    print(err)
-  cmd = 'git ls-remote https://github.com/hexbat/test_self_update.git HEAD'
-  p = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
-  out, err = p.communicate()
-  if out:
-    remote_hash = out.split()[0]
-  if err:
-    print('Error while getting remote hash')
-    print(err)
-  
-  print(local_hash)
-  print(remote_hash)
-  
+	remote_hash = ''
+	local_hash = ''
+	error_code = 0
+	cmd = 'git rev-parse HEAD'
+	p = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
+	out, err = p.communicate()
+	if out:
+ 		local_hash = out.split()[0]
+	if err:
+		print('Error while getting local hash')
+		print(err)
+		error_code += 1
+	cmd = 'git ls-remote https://github.com/hexbat/test_self_update.git HEAD'
+	p = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
+	out, err = p.communicate()
+	if out:
+		remote_hash = out.split()[0]
+	if err:
+		print('Error while getting remote hash')
+		print(err)
+		error_code += 1
+	print(local_hash)
+	print(remote_hash)
+	if (local_hash != remote_hash) and (error_code == 0):
+		print('Found update')
+	
 
 if __name__ == "__main__":
-    main()
+	main()
 
