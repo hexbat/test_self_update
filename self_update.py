9ee38d8f31f96ce61fdf9dc3f40d1d5630c1b209
hexbat <hexbat@gmail.com>
2016-09-15 14:01:35 +0300

    print

--- self_update.py
+++ self_update.py
@@ -11,17 +11,17 @@ def main():
   if out:
     local_hash = out
   if err:
-    print 'Error while getting local hash'
+    print('Error while getting local hash')
   cmd = 'git ls-remote https://github.com/hexbat/test/test_self_update.git HEAD'
   p = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
   out, err = p.communicate()
   if out:
     remote_hash = out
   if err:
-    print 'Error while getting remote hash'
+    print('Error while getting remote hash')
   
-  print local_hash
-  print remote_hash
+  print(local_hash)
+  print(remote_hash)
   
 
 if __name__ == "__main__":
